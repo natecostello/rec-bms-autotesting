e4916abb8b99625754df84b864b9e9348113c962
Nate Costello <natecostello@gmail.com>
2022-03-06 15:50:52 +0000

    workaround to enable p/s via software

--- bms_test/pre_charge-script.py
+++ bms_test/pre_charge-script.py
@@ -13,11 +13,11 @@ from standard_setup import BMSTestFixture
 from datetime import datetime
 
 """This script leverages BMSTestFixture to record parameters during the initial battery precharge performed prior to a parallel cell top-balance."""
-"""All Power supply parameters are set manually, including enable.  This script only monitors."""
+"""All Power supply parameters are set manually, except output enable (bug).  This script only monitors."""
 """CMAX 'End of charge hysterisis per cell' MUST be changed from the default 3.85 to 3.65 prior to initial battery precharge."""
 """MAXH 'Over-voltage switch-off hysteresis per cell' SHOULD be changed from the default 0.25 to 0.05."""
 """Riden voltage setpoint should be 29.2V (For a subsequent 3.65V top balance)"""
-"""Rident current setpoint should be 18A"""
+"""Rident current setpoint should be 15A (for p/s margin)"""
 """DMM Should be set to measure voltage across the battery pack"""
 """O-scope will be used to measure voltage ripple across battery pack"""
 """Contactor Aux Contacts should be wired to logging pi 3.3v and to contactor input pin"""
@@ -37,11 +37,15 @@ time.sleep(1)
 test_fixture.logger.filename = 'initial-battery-precharge'
 test_fixture.logger.start()
 
-# User has 60 seconds to turn on system and enable riden, otherwise logging will stop, and script will end
-print("Turn on system via main switch and enable Riden output.")
+# User has 30 seconds to turn on system and enable riden, otherwise logging will stop, and script will end
+print("Turn on system via main switch.")
 
-time.sleep(60)
+time.sleep(30)
 
+if test_fixture.bin_monitor.contactor:
+    time.sleep(1)
+    test_fixture.powersupply.set_output(True)
+    
 # Log until BMS opens contactor or output is disabled
 
 while test_fixture.bin_monitor.contactor and test_fixture.powersupply.is_output:
